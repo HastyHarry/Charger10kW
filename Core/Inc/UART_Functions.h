b932022c4d47215702811c5af8e8742577fc75f1
HastyHarry <44320528+HastyHarry@users.noreply.github.com>
2021-06-30 19:53:33 +0300

    update 3006 vol.2

---
+++ Core/Inc/UART_Functions.h
@@ -0,0 +1,43 @@
+/**
+ * @file    PWM_Functions.h
+ * @author  Antushev N.
+ * @version V1.0.0
+ * @date    16-Jun-2021
+ * @brief   .
+ * @details
+ *
+ *
+ */
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __UART_FUNCTIONS_H
+#define __UART_FUNCTIONS_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "Application_Conf.h"
+
+typedef struct{
+	uint8_t Header1;
+	uint8_t Header2;
+	uint8_t Source;
+	uint8_t Command_type;
+	uint16_t Addr[5];
+	uint16_t Data[5];
+	uint8_t DataLength;
+
+}UART_Message_Struct;
+
+typedef enum{
+	UART_MSG_Types_READ = 01,
+	UART_MSG_Types_Write = 02,
+	UART_MSG_Types_WriteConfirm = 0x22
+}UART_MSG_Types;
+  
+void UART_Message_Init(UART_Message_Struct *MSG_SRC_Struct, uint16_t Header, uint8_t Source_Addr);
+void UART_Message_Send(UART_Message_Struct *MSG_SRC_Struct, uint16_t Addresses[5], uint16_t Data[5], UART_MSG_Types Command, uint8_t HowMuchData);
+void UART_Message_Send_Package_Processing(UART_Message_Struct *MSG_SRC_Struct,DMA_UART_STRUCT *UART_Cooked);
+void UART_Received_Message_Processing(UART_Message_Struct *MSG_SRC_Struct, DMA_UART_STRUCT *SOURCE );
+
+#endif
---
+++ Core/Src/UART_Functions.c
@@ -0,0 +1,71 @@
+/**
+******************************************************************************
+* @file           : PWM_Functions.c
+* @brief          : Functions for PWM control using G334
+******************************************************************************
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "UART_Functions.h"
+
+
+void UART_Message_Init(UART_Message_Struct *MSG_SRC_Struct, uint16_t Header, uint8_t Source_Addr){
+	MSG_SRC_Struct->Header1 = Lowest(Header);
+	MSG_SRC_Struct->Header1 = Low(Header);
+	MSG_SRC_Struct->Source = Source_Addr;
+}
+
+void UART_Message_Send(UART_Message_Struct *MSG_SRC_Struct, uint16_t Addresses[5], uint16_t Data[5], UART_MSG_Types Command, uint8_t HowMuchData)
+{
+	uint8_t i;
+	switch (Command){
+	case UART_MSG_Types_READ:
+		MSG_SRC_Struct->Command_type = UART_MSG_Types_READ;
+		MSG_SRC_Struct->DataLength = HowMuchData;
+		for(i=0;i<HowMuchData;i++){
+			MSG_SRC_Struct->Addr[i]=Addresses[i];
+		}
+	break;
+	case UART_MSG_Types_Write:
+
+	break;
+	case UART_MSG_Types_WriteConfirm:
+
+	break;
+	}
+
+}
+
+void UART_Message_Send_Package_Processing(UART_Message_Struct *MSG_SRC_Struct,DMA_UART_STRUCT *UART_Cooked){
+
+	UART_Cooked->Transmit[0] = MSG_SRC_Struct->Header1;
+	UART_Cooked->Transmit[1] = MSG_SRC_Struct->Header2;
+	UART_Cooked->Transmit[2] = MSG_SRC_Struct->Source;
+	UART_Cooked->Transmit[3] = MSG_SRC_Struct->Command_type;
+	UART_Cooked->Transmit[4] = MSG_SRC_Struct->DataLength;
+	UART_Cooked->Transmit[5] = Lowest(MSG_SRC_Struct->Addr[0]);
+	UART_Cooked->Transmit[6] = Low(MSG_SRC_Struct->Addr[0]);
+	UART_Cooked->Transmit[7] = Lowest(MSG_SRC_Struct->Data[0]);
+	UART_Cooked->Transmit[8] = Low(MSG_SRC_Struct->Data[0]);
+	UART_Cooked->Transmit[9] = Lowest(MSG_SRC_Struct->Addr[1]);
+	UART_Cooked->Transmit[10] = Low(MSG_SRC_Struct->Addr[1]);
+	UART_Cooked->Transmit[11] = Lowest(MSG_SRC_Struct->Data[1]);
+	UART_Cooked->Transmit[12] = Low(MSG_SRC_Struct->Data[1]);
+	UART_Cooked->Transmit[13] = Lowest(MSG_SRC_Struct->Addr[2]);
+	UART_Cooked->Transmit[14] = Low(MSG_SRC_Struct->Addr[2]);
+	UART_Cooked->Transmit[15] = Lowest(MSG_SRC_Struct->Data[2]);
+	UART_Cooked->Transmit[16] = Low(MSG_SRC_Struct->Data[2]);
+	UART_Cooked->Transmit[17] = Lowest(MSG_SRC_Struct->Addr[3]);
+	UART_Cooked->Transmit[18] = Low(MSG_SRC_Struct->Addr[3]);
+	UART_Cooked->Transmit[19] = Lowest(MSG_SRC_Struct->Data[3]);
+	UART_Cooked->Transmit[20] = Low(MSG_SRC_Struct->Data[3]);
+	UART_Cooked->Transmit[21] = Lowest(MSG_SRC_Struct->Addr[4]);
+	UART_Cooked->Transmit[22] = Low(MSG_SRC_Struct->Addr[4]);
+	UART_Cooked->Transmit[21] = Lowest(MSG_SRC_Struct->Data[4]);
+	UART_Cooked->Transmit[22] = Low(MSG_SRC_Struct->Data[4]);
+
+}
+
+void UART_Received_Message_Processing(UART_Message_Struct *MSG_SRC_Struct, DMA_UART_STRUCT *SOURCE ){
+
+}
